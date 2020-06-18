84c041a6aa06dc2070d9f05a44fadf3a88837530
zzlei3082 <1094526056@qq.com>
2020-06-18 17:16:41 +0800

    测试提交

---
+++ module01/src/com/zznode/test/Test.java
@@ -0,0 +1,8 @@
+package com.zznode.test;
+
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("helloWorld2");
+    }
+}
---
+++ src/com/test/java/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.test.java;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("helloworld");
+    }
+}
